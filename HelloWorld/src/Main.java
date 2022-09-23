e27efd7bb0956b43a8a6765601b0dd4011cc8afe
ReneLouis <rg.louis@hotmail.fr>
2022-09-23 07:19:27 +0100

    created new folder 'testApps' to be pushed to new branch.

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -5,19 +5,23 @@ public class Main {
         System.out.println("I am computer, What is your name?");
         String name = scanner.nextLine();
 
-        System.out.printf("Howdy %s, How many springs old are you?", name);
+        System.out.printf("Howdy %s, How many springs old are you?\n", name);
         int age = scanner.nextInt();
+
+        // consumes the dangling new line character
+        scanner.nextLine();
+
         if (age < 18) {
             System.out.println(String.format("Well, I do surely hope you've had fun during those %d years! Sadly you'll need a few more to access this app.", age));
         } else if (age >= 55) {
             System.out.printf("Wait, you really are %d? You can access the app but you may be a bit old for it..", age);
         } else {
-            System.out.printf("Okay, %d is an acceptable age to access this app, so enjoy %s. \nWhat bike have you got?", age, name);
+            System.out.printf("Okay, %d is an acceptable age to access this app, so enjoy %s. \nWhat bike have you got?\n", age, name);
             String bikeModel = scanner.nextLine();
 
-            System.out.printf("What the engine size of %s?", bikeModel);
-            //short size = scanner.nextShort();
-            //System.out.printf("So to resume, you are %s, %d years old, and ride a %dcc %s, nice one!", name, age, size, bike);
+            System.out.printf("What the engine size of %s?\n", bikeModel);
+            short size = scanner.nextShort();
+            System.out.printf("So to resume, you are %s, %d years old, and ride a %dcc %s, nice one!", name, age, size, bikeModel);
         }
 
 
